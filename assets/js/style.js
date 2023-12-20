9c3dc680665a3c0315c236ced60b1afd4c163e7b
johan-kindgren <>
2023-12-20 19:14:30 +0000

    edit js stll mark red

--- assets/js/style.js
+++ assets/js/style.js
@@ -90,59 +90,60 @@ document.addEventListener('DOMContentLoaded', () => {
         percentageTextElement.textContent = `Current Progress: ${progressPercentage.toFixed(2)}%`;
     }
 
+    // Sample questions (replace with your own)
     function getQuestions() {
-
-        question: "What is the capital of Sweden?",
-            answers: [
-                { text: "Gothenburg", correct: false },
-                { text: "Malmo", correct: false },
-                { text: "Uppsala", correct: false },
-                { text: "Stockholm", correct: true }
-            ];
-    },
-    {
-        question: "Which Swedish company is known for flat-pack furniture?",
-            answers: [
-                { text: "Volvo", correct: false },
-                { text: "IKEA", correct: true },
-                { text: "H&M", correct: false },
-                { text: "Ericsson", correct: false }
-            ];
-    },
-    {
-        question: "What is the traditional Swedish festival celebrating the summer solstice?",
-            answers: [
-                { text: "Valborg", correct: false },
-                { text: "Midsommar", correct: true },
-                { text: "Jul", correct: false },
-                { text: "Påsk", correct: false }
-            ];
-    },
-    {
-        question: "Which of these is a traditional Swedish dish?",
-            answers: [
-                { text: "Köttbullar (meatballs)", correct: true },
-                { text: "Pizza", correct: false },
-                { text: "Sushi", correct: false },
-                { text: "Hamburger", correct: false }
-            ];
-    },
-    {
-        question: "Which city is known for its well-preserved medieval city center?",
-            answers: [
-                { text: "Gothenburg", correct: false },
-                { text: "Uppsala", correct: false },
-                { text: "Visby", correct: true },
-                { text: "Lund", correct: false }
-            ];
-    }
-}
+        return [
+            {
+                question: "What is the capital of Sweden?",
+                answers: [
+                    { text: "Gothenburg", correct: false },
+                    { text: "Malmo", correct: false },
+                    { text: "Uppsala", correct: false },
+                    { text: "Stockholm", correct: true }
+                ];
+            },
+            {
+                question: "Which Swedish company is known for flat-pack furniture?",
+                answers: [
+                    { text: "Volvo", correct: false },
+                    { text: "IKEA", correct: true },
+                    { text: "H&M", correct: false },
+                    { text: "Ericsson", correct: false }
+                ];
+            },
+            {
+                question: "What is the traditional Swedish festival celebrating the summer solstice?",
+                answers: [
+                    { text: "Valborg", correct: false },
+                    { text: "Midsommar", correct: true },
+                    { text: "Jul", correct: false },
+                    { text: "Påsk", correct: false }
+                ];
+            },
+            {
+                question: "Which of these is a traditional Swedish dish?",
+                answers: [
+                    { text: "Köttbullar (meatballs)", correct: true },
+                    { text: "Pizza", correct: false },
+                    { text: "Sushi", correct: false },
+                    { text: "Hamburger", correct: false }
+                ];
+            },
+            {
+                question: "Which city is known for its well-preserved medieval city center?",
+                answers: [
+                    { text: "Gothenburg", correct: false },
+                    { text: "Uppsala", correct: false },
+                    { text: "Visby", correct: true },
+                    { text: "Lund", correct: false }
+                ];
+            }
 
     // Event Listeners
     startQuizButton.addEventListener('click', startQuiz);
-restartQuizButton.addEventListener('click', startQuiz);
-submitAnswerButton.addEventListener('click', submitAnswer);
+        restartQuizButton.addEventListener('click', startQuiz);
+        submitAnswerButton.addEventListener('click', submitAnswer);
 
-// Start the quiz
-startQuiz();
-});
+        // Start the quiz
+        startQuiz();
+    });
